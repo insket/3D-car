cde2768d1a645782d1597cda8146456d9b5b1bb6
insket <2694183181@qq.com>
2023-08-25 11:23:01 +0800

    feat(box):add box float

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,7 +1,11 @@
 import { CubeCamera, Environment, OrbitControls, PerspectiveCamera } from "@react-three/drei";
+import { EffectComposer, Bloom, ChromaticAberration } from "@react-three/postprocessing";
+import { BlendFunction } from "postprocessing";
 import Ground from "./Ground";
 import Car from "./Car";
 import Rings from "./Rings";
+import Boxes from "./Box";
+import FloatingGrid from "./FloatingGrid";
 
 export const Experience = () => {
   return (
@@ -26,6 +30,10 @@ export const Experience = () => {
 
       <Rings />
 
+      <Boxes />
+
+      {/* <FloatingGrid /> */}
+
       <spotLight
         color={[1, 0.25, 0.7]}
         intensity={1.5}
@@ -45,6 +53,20 @@ export const Experience = () => {
         castShadow
         shadow-bias={-0.0001}
       />
+
+      <EffectComposer>
+        {/* <Bloom
+          blendFunction={BlendFunction.ADD}
+          intensity={1.3}
+          width={300}
+          height={300}
+          kernelSize={5}
+          luminanceThreshold={0.15}
+          luminanceSmoothing={0.025}
+        /> */}
+
+        <ChromaticAberration blendFunction={BlendFunction.NORMAL} offset={[0.0005, 0.0012]} />
+      </EffectComposer>
     </>
   );
 };
